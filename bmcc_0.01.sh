a5896d89c096b62721449c43ae86c7f266043561
navyblue1993 <john245678@gmail.com>
2022-11-26 16:18:07 -0500

    extra files

--- bmcc_0.01.sh
+++ bmcc_0.01.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 module load anaconda3/2020.11
 conda activate /ocean/projects/tra220029p/tjin1/anaconda3/envs/e2e-asr
-python train.py --conf conf/bmcc_0.001.yaml --tag bmcc_0.001_lr1e-3_ep60_mcc
+python train.py --conf conf/bmcc_0.01.yaml --tag bmcc_0.01_lr1e-3_ep60_mcc
